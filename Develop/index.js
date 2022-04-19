c86e4b0181d05c81f1b57e224d275fc82e2dba1d
Beimy <arbm159357@gmail.com>
2022-04-19 11:50:01 -0400

    writing basic readme file from js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,28 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+    //page we are building then passing into writefile
+    const pageHTML = generateMarkdown(data);
+
+    fs.writeFile('./YOUR-README.md', pageHTML, err =>{
+        if(err) throw new Error(err);
+
+        console.log('ReadMe Generated');
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile('README GENREATOR', 'README GENERATOR');
+}
 
 // Function call to initialize app
 init();
